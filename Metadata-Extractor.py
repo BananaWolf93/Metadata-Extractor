d4ec1a1de91d3fb424f519dfbcb27beb882e2aca
BananaWolf93 <christopherkentleal@gmail.com>
2023-12-17 13:45:20 -0700

    Initial Commit

---
+++ Metadata-Extractor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+from tika import parser
+
+user_input = input("Please enter the absolute path for the file you'd like to analyze: ")
+file_path = user_input
+parsed = parser.from_file(file_path)
+
+metadata = parsed.get("metadata, {}")
+for key, value in metadata.items():
+    print (f"{key}: {value}")
+
+
+
