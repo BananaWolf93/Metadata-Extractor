d77e23d35ef8143a904fdd5ab13687f6619ae222
BananaWolf93 <christopherkentleal@gmail.com>
2023-12-18 16:24:26 -0700

    Improved script.

--- Metadata-Extractor.py
+++ Metadata-Extractor.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python3
 
-# The following libraries are first imported:
+# Needed libraries:
 
 import os
 from tika import parser
 
-# The following variables are needed for the identification of the user's input and then checking for path validation and a value to send to parse the file:
-
-user_input = input("Please enter the absolute path for the file you'd like to analyze: ")
-file_path = user_input
-parsed = parser.from_file(file_path)
-
-# The following function grabs the user's input and determines if it is valid or not. If valid, the user will be notified then the script will complete by extracting all metadata found.
-# Otherwise, if invalid, the user will also be notified and advised to recheck the absolute path of the targeted file.
+# Defining a function to grab the user's input, determine the value as the file path and passing it along to parse after it has been verified and confirmed as "valid".
 
 def data_extraction():
+    user_input = input("Please enter the absolute path for the file you'd like to analyze: ")
+    file_path = user_input
+
     if os.path.exists(file_path):
-        print("Path verified! Returning results... ")
-        metadata = parsed.get("metadata", {})
-        for key, value in metadata.items():
-            print (f"{key}: {value}")
+        try:
+            parsed = parser.from_file(file_path)
+            print("Path verified! Attempting to extract metadata...")
+            
+            metadata = parsed.get("metadata", {})
+            if metadata:
+                for key, value in metadata.items():
+                    print(f"{key}: {value}")
+            else:
+                print("No metadata found in the file.")
+                
+        except Exception as e:
+            print(f"An error occurred during parsing: {e}")
+            
     else:
         print("Path NOT valid, please check the absolute path for the target and try again.")
-    return file_path
+
+data_extraction()
